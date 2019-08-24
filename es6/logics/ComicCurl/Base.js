7de7bf296ac51bea4a456f1ea14d502a3c4eeaca
云天河 <hlzblog@vip.qq.com>
2019-08-25 02:55:52 +0800

    初版完成

--- es6/logics/ComicCurl/Base.js
+++ es6/logics/ComicCurl/Base.js
@@ -90,7 +90,7 @@ export default class Base {
             datas.push({
                 sequence,
                 page_id,
-                "src": imgs,
+                "src": img,
                 
             })
         }
