c3fd9fa49b3e63c281da4d3c9ebf8f44e3c757e9
云天河 <hlzblog@vip.qq.com>
2019-08-29 11:32:12 +0800

    删除无用函数

--- es6/services/Comic/Base.js
+++ es6/services/Comic/Base.js
@@ -2,15 +2,7 @@ import BaseService from '../../libs/Base/BaseService'
 import Page from '../../models/Comic/Page'
 import Image from '../../models/Comic/Image'
 
-
-const CHANNEL_ID_LIST = {
-    'ManHuaNiu': 2 // Âþ»­Å£
-}
-
 class Base extends BaseService {
-    static get_channel(channel_name) {
-        return CHANNEL_ID_LIST[channel_name]
-    }
     static async insert_pages(datas) {
         let {rows, first_insert_id} = await Page.insert(datas)
         return {rows, first_insert_id}
