9a4f7b7929cc25ed41a5cdbb6e6bf0fe45f803fe
云天河 <hlzblog@vip.qq.com>
2019-08-30 10:13:17 +0800

    修改历史遗留备注信息

--- es6/app.js
+++ es6/app.js
@@ -5,11 +5,11 @@
 // GITHUB: https://github.com/HaleyLeoZhang
 // ----------------------------------------------------------------------
 import Register from './libs/Register'
-// 带注册模块列表
+// 待注册模块列表
 import ComicCurl from './cli/ComicCurl'
 
 const app = new Register()
 app.bootstrap()
     // 注册模块，调用的模块方法，都得是 async
-    .use('comic', ComicCurl) // 调用示例 node ./app.js  comic start_mhn
+    .use('comic', ComicCurl) // 调用示例 node ./app.js  comic mhn_pages
     .run()
