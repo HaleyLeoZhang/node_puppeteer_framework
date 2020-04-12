ef91f4ebc8235a30510d23285e84421e6cc98bfb
云天河 <hlzblog@vip.qq.com>
2020-04-12 17:53:24 +0800

    已解决MQ相关问题

--- es6/cli/Task/ManhuaNiuTask.js
+++ es6/cli/Task/ManhuaNiuTask.js
@@ -6,8 +6,9 @@
 // GITHUB: https://github.com/HaleyLeoZhang
 // ----------------------------------------------------------------------
 import ManHuaNiuLogic from '../../logics/ComicCurl/ManHuaNiuLogic'
-import RabbitMQ from '../../libs/MQ/RabbitMQ'
+import RabbitMQ, { ACK_YES, ACK_NO } from '../../libs/MQ/RabbitMQ'
 import Log from '../../tools/Log'
+import General from '../../tools/General'
 
 // ----------------------------------------------------------------------
 //      业务枚举
@@ -19,10 +20,12 @@ import Log from '../../tools/Log'
 const ACTION_CHAPTER_LIST = 'chapter_list'; // 章节列表
 const ACTION_IMAGE_LIST = 'image_list'; // 漫画图片
 /**
- * @var bool 是否返回ACK
+ * @var string 队列配置
  */
-const ACK_YES = true; // 返回 ACK
-const ACK_NO = false; // 不返回 ACK
+const AMQP_EXCHANGE = 'amq.topic'; // 交换机
+const AMQP_ROUTING_KEY = 'comic_manhuaniu_sync'; // 路由规则
+const AMQP_QUEUE = 'comic_manhuaniu_sync_queue'; // 队列名
+const DELAY_SECNOD = 30; // 无消息时,需要挂起的秒数
 
 // ----------------------------------------------------------------------
 //      业务逻辑
@@ -42,15 +45,15 @@ export default class ManhuaNiuTask {
         //     "body":{}, // 其他参数
         // };
         const mq = new RabbitMQ();
-        mq.set_exchange("amq.topic")
-        mq.set_routing_key("comic_manhuaniu_sync")
-        mq.set_queue("comic_manhuaniu_sync_queue")
-        mq.pull(async (payload, channel) => {
-            if(ACK_YES === await this.dispatch(payload)){
-                channel.ack(msg); // 发送确认的ACK
-            }
+        mq.set_exchange(AMQP_EXCHANGE)
+        mq.set_routing_key(AMQP_ROUTING_KEY)
+        mq.set_queue(AMQP_QUEUE)
+        mq.pull(async (payload) => {
+            await this.delay_ms(General.mt_rand(500, 3000)); // 随机停顿毫秒数,防止消费过快
+            return await this.dispatch(payload)
         })
     }
+
     /**
      * 任务分发
      * @param JSON_Object payload 约定入参
@@ -58,35 +61,67 @@ export default class ManhuaNiuTask {
      */
     static async dispatch(payload) {
         let ack_flag = ACK_YES;
-        switch (payload.action) {
-            case ACTION_CHAPTER_LIST:
-                ack_flag = await ManHuaNiuLogic.getChapterList(payload);
-                break;
-            case ACTION_IMAGE_LIST:
-                ack_flag = await ManHuaNiuLogic.getImageList(payload);
-                break;
-            default:
-                Log.error('ACTION_ERROR: ', JSON.stringify(payload))
+        try {
+            switch (payload.action) {
+                case ACTION_CHAPTER_LIST:
+                    ack_flag = await ManHuaNiuLogic.getChapterList(payload);
+                    break;
+                case ACTION_IMAGE_LIST:
+                    ack_flag = await ManHuaNiuLogic.getImageList(payload);
+                    break;
+                default:
+                    Log.error('ACTION_ERROR: ', JSON.stringify(payload))
+            }
+        } catch (err) {
+            ack_flag = ACK_NO
+            Log.error('ManhuaNiuTask.CONSUME_ERROR: ', err.message)
         }
         return ack_flag
     }
 
+    static delay_ms(ms) {
+        new Promise(resolve => setTimeout(() => resolve(), ms));
+    }
+
     /**
-     * 拉取漫画相关信息
-     * - 漫画牛渠道用同一个队列
+     * 拉
      */
-    static async test() {
-        // // 漫画场景约定格式
+    static async test_pull() {
+        const mq = new RabbitMQ();
+        mq.set_exchange(AMQP_EXCHANGE)
+        mq.set_routing_key(AMQP_ROUTING_KEY)
+        mq.set_queue(AMQP_QUEUE)
+        mq.set_delay_second(DELAY_SECNOD)
+        // 拉
+        await mq.pull(async (payload) => {
+            console.log('payload', payload)
+            // let payload_1 = {
+            //     "id": 4444, // 漫画书架ID 表 comic.id 
+            //     "url": "", // 页面 URL
+            //     "action": "", // 采集动作
+            //     "body": {}, // 其他参数
+            // };
+            // console.log('do')
+            return ACK_YES;
+        })
+    }
+
+    /**
+     * 推
+     */
+    static async test_push() {
+        const mq = new RabbitMQ();
+        mq.set_exchange(AMQP_EXCHANGE)
+        mq.set_routing_key(AMQP_ROUTING_KEY)
+        mq.set_queue(AMQP_QUEUE)
+
+        // 推
         let payload = {
-            "id": 0, // 漫画书架ID 表 comic.id 
+            "id": 544, // 漫画书架ID 表 comic.id 
             "url": "", // 页面 URL
             "action": "", // 采集动作
-            "body":{}, // 其他参数
+            "body": {}, // 其他参数
         };
-        const mq = new RabbitMQ();
-        mq.set_exchange("/")
-        mq.set_routing_key("comic_manhuaniu_sync")
-        mq.set_queue("comic_manhuaniu_sync_queue")
-        mq.push(payload)
+        await mq.push(payload)
     }
 }
--- es6/libs/MQ/RabbitMQ.js
+++ es6/libs/MQ/RabbitMQ.js
@@ -1,8 +1,11 @@
 // ----------------------------------------------------------------------
 // RabbitMQ 驱动
 // ----------------------------------------------------------------------
-// 文档地址 https://github.com/squaremo/amqp.node
-// RabbitMQ对应node文档地址 https://www.rabbitmq.com/tutorials/tutorial-three-javascript.html
+// 运维组件-安装文档 https://blog.csdn.net/nextyu/article/details/79250174
+// Node包-官方文档地址 https://github.com/squaremo/amqp.node
+// Node包 对应API http://www.squaremobius.net/amqp.node/channel_api.html
+// 源自掘金用法 https://juejin.im/post/5dd8cd7ae51d4523501f7331
+// RabbitMQ官网解释使用 https://www.rabbitmq.com/tutorials/tutorial-three-javascript.html
 // ----------------------------------------------------------------------
 // Link  : http://www.hlzblog.top/
 // GITHUB: https://github.com/HaleyLeoZhang
@@ -13,85 +16,183 @@ import { DSN_AMQP } from '../../conf/mq/amqp'
 import Log from '../../tools/Log'
 
 const Buffer = require('safe-buffer').Buffer;
-const DEFAULT_OPTION = {durable: true, autoDelete:false};
+const DEFAULT_OPTION = { durable: true, autoDelete: false };
+const DEFAULT_CONSUME_OPTION = { noAck: false };
+const DEFAULT_DELAY_SECOND = 3; // 未拉取到消息时,默认挂起秒数
+const FLAT_NO_MESSAGE = false; // 没有消息的时候,返回值是false
+/**
+ * @var bool 是否返回ACK
+ */
+const ACK_YES = true; // 返回 ACK
+const ACK_NO = false; // 不返回 ACK
 
 export default class RabbitMQ {
-    get_conn() {
-        // amqp://user:pass@localhost:1000/vhost
-        const Dial = `amqp://${DSN_AMQP.user}:${DSN_AMQP.password}@${DSN_AMQP.host}:${DSN_AMQP.port}${DSN_AMQP.vhost}`
-        console.log('Dial ', Dial)
-        return amqp.connect(Dial)
-    }
-    // 设置交换机 exchange
+    /**
+     * 设置交换机
+     *
+     * @param string exchange 交换机名
+     * @return void
+     */
     set_exchange(exchange) {
         this.exchange = exchange
     }
-    // 设置路由名 routingKey
+    /**
+     * 设置路由名
+     *
+     * @param string routing_key 路由绑定规则名
+     * @return void
+     */
     set_routing_key(routing_key) {
         this.routing_key = routing_key
     }
-    // 设置队列名 queue
+    /**
+     * 设置队列名
+     *
+     * @param string queue 队列名
+     * @return void
+     */
     set_queue(queue) {
         this.queue = queue
     }
-    // 消费者
-    async pull(callback) {
-        //
+    /**
+     * 设置挂起秒数
+     *
+     * @param int delay_second 延迟秒数
+     * @return void
+     */
+    set_delay_second(delay_second) {
+        this.delay_second = delay_second
     }
-    // 生产者
-    async push(payload) {
-        // Publisher
-        this.get_conn().then(function(conn) {
-            console.log('22')
-            console.log(conn)
-            return conn.createChannel();
-        }).then(function(ch) {
-            console.log('44')
-            return ch.assertQueue(this.queue, DEFAULT_OPTION).then(function(ok) {
-                return ch.sendToQueue(this.queue, Buffer.from('something to do'));
-            });
-        }).catch(console.warn);
-        // 1.创建链接对象
-        const connection = this.get_conn()
-
-        console.log(connection)
-        console.log('55')
-        // 2. 获取通道
-        const channel = await connection.createChannel();
-        console.log('66')
+    /**
+     * 获取挂起秒数
+     *
+     * @return void
+     */
+    get_delay_second() {
+        return this.delay_second || DEFAULT_DELAY_SECOND
+    }
+    /**
+     * 获取连接
+     *
+     * @param string queue 队列名
+     * @return void
+     */
+    get_conn() {
+        const Dial = `amqp://${DSN_AMQP.user}:${DSN_AMQP.password}@${DSN_AMQP.host}:${DSN_AMQP.port}${DSN_AMQP.vhost}`
+        return amqp.connect(Dial)
+    }
+    /**
+     * 单例获取连接信息,但每次新建通道
+     *
+     * @param string queue 队列名
+     * @return void
+     */
+    async prepare() {
+        if (!this.conn) {
+            // 创建链接对象
+            this.conn = await this.get_conn()
+        }
+        // 获取通道
+        // - 信道本身的流量很大时,这时候多个信道复用一个 Connection 就会产生性能瓶颈，进而使整体的流量被限制了
+        // - 就目前来看,一个连接上,不会有太大数据量
+        const channel = await this.conn.createChannel();
         // - 声明(初始化)队列
         await channel.assertQueue(this.queue, DEFAULT_OPTION)
         // - 声明(初始化)队列与交换机的路由关系
-        await channel.bindQueue(this.queue, this.exchange, this.routing_key, null);
+        await channel.bindQueue(this.queue, this.exchange, this.routing_key, null)
+        return { "conn": this.conn, channel }
+    }
+    /**
+     * 消费者一条条消费
+     *
+     * @param callable callback 消费者回调函数
+     * @return void
+     */
+    async pull(callback) {
+        const { conn, channel } = await this.prepare()
 
-        console.log('77')
+        for (;;) {
+            await new Promise(resolve => {
+                channel.get(this.queue, DEFAULT_CONSUME_OPTION)
+                    .then(msg => {
+                        if (FLAT_NO_MESSAGE === msg) {
+                            // 没有消息就挂起
+                            setTimeout(() => {
+                                resolve()
+                            }, this.get_delay_second() * 1000);
+                        } else {
+                            let payload = msg.content.toString()
+                            callback(JSON.parse(payload))
+                                .then((ack_yes) => {
+                                    if (ACK_YES === ack_yes) {
+                                        channel.ack(msg);
+                                    } else {
+                                        channel.nack(msg);
+                                    }
+                                    resolve()
+                                })
+                        }
+                    });
 
-        // 3. 声明参数
-        const msg = 'hello koala';
-
-        for (let i = 0; i < 10000; i++) {
-            // 4. 发送消息
-            let str = `${msg} 第${i}条消息`;
-            await channel.publish(this.exchange, this.routing_key, Buffer.from(str)); // 发送到交换机
-            console.log(str)
+            });
         }
-
-        // 5. 关闭通道
         await channel.close();
-        // 6. 关闭连接
-        await connect.close();
-
-        // amqp.connect(this.get_dial(), (err, conn) => {
-        // console.log("222");
-        //     conn.createChannel((err, ch) => {
-        //         let msg = 'Hello World!';
-
-        //         channel.assertQueue(this.queue, { durable: true, autoDelete: false });
-        //         channel.sendToQueue(this.queue, new Buffer(msg)); //   发送消息
-        //         console.log("Send message:", msg);
-        //     });
-        //     setTimeout(function() { conn.close();
-        //         process.exit(0) }, 500);
-        // });
+        await conn.close();
+    }
+    /**
+     * 消费者批量拉所有消息下来,并发消费
+     * - 适用于生产与消费速度差不多的场景
+     * - 注: 当前场景-暂不使用
+     *
+     * @param callable callback 消费者回调函数
+     * @return void
+     */
+    async pull_batch(callback) {
+        const { conn, channel } = await this.prepare()
+        await new Promise((resolve) => { // 挂起消费
+            channel.consume(this.queue, async (msg) => {
+                if (FLAT_NO_MESSAGE === msg) {
+                    resolve(true)
+                }
+                let payload = msg.content.toString()
+                await callback(JSON.parse(payload))
+                    .then((ack_yes) => {
+                        if (ACK_YES === ack_yes) {
+                            channel.ack(msg);
+                        } else {
+                            channel.nack(msg);
+                        }
+                    })
+            }, DEFAULT_CONSUME_OPTION)
+        })
+        await channel.close();
+        await conn.close();
+        // console.log('关闭')
+    }
+    /**
+     * 在消费者内 给当前队列 入队消息
+     *
+     * @param JSON_Object payload 消息内容
+     * @return void
+     */
+    async append(payload) {
+        const { conn, channel } = await this.prepare()
+        // 发送消息到交换机
+        await channel.publish(this.exchange, this.routing_key, Buffer.from(JSON.stringify(payload)))
+        await channel.close();
+    }
+    /**
+     * 生产者
+     *
+     * @param JSON_Object payload 消息内容
+     * @return void
+     */
+    async push(payload) {
+        const { conn, channel } = await this.prepare()
+        // 发送消息到交换机
+        await channel.publish(this.exchange, this.routing_key, Buffer.from(JSON.stringify(payload)))
+        await channel.close();
+        await conn.close();
     }
 }
+export { ACK_YES, ACK_NO }
