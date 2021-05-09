22047a06903e9b5845aba211e02d5ec14504ffb4
BD-HD1-BUSINESS <hlzblog@vip.qq.com>
2021-05-09 12:50:56 +0800

    update

---
+++ docker/ini.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export TERM=xterm \
+&& apt-get update \
+&& apt-get install supervisor -y  \
+&& cd /app \
+&& make install \
+&& /usr/bin/supervisord 
+
+for((;;))
+do
+    # 定时通知抓取
+    echo "推送中"
+    /usr/local/bin/node /app/es5/task.js comic notify_sub_all
+    echo "推送成功"
+    sleep_second=60
+    echo "正在挂起 ${sleep_second} 秒"
+    sleep ${sleep_second}
+done
+
+
+# 挂起容器实例
+tail -f 
