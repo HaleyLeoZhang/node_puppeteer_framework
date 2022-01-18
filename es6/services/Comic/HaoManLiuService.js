095182f3d33654898c8e904a88d360ccf6cec907
沐临风 <hlzblog@vip.qq.com>
2022-01-18 22:59:48 +0800

    update

--- es6/services/Comic/HaoManLiuService.js
+++ es6/services/Comic/HaoManLiuService.js
@@ -9,7 +9,7 @@ const cheerio = require('cheerio'); // html解析器 文档 https://www.npmjs.co
 const BASE_HREF = "https://www.g-lens.com" // 爬取地址
 
 export default class HaoManLiuService extends Base {
-    static get_base_href(){
+    static get_base_href() {
         return BASE_HREF
     }
 
@@ -33,7 +33,7 @@ export default class HaoManLiuService extends Base {
                 let name = $(".j-comic-title").text()
                 let pic = $(".de-info__cover img").attr("src")
                 let intro = $(".intro-total").text()
-                Log.ctxInfo(ctx,  JSON.stringify({name, pic, intro}))
+                Log.ctxInfo(ctx, JSON.stringify({name, pic, intro}))
                 Log.ctxInfo(ctx, `拉取结束 source_id ${source_id} 基本信息`)
                 return {name, pic, intro}
             })
@@ -45,7 +45,7 @@ export default class HaoManLiuService extends Base {
     static async get_chapter_list(ctx, source_id) {
         let _this = this
         Log.ctxInfo(ctx, `开始拉取 source_id ${source_id} 列表信息-头部`)
-        let target_url = `${BASE_HREF}/${source_id}/`
+        let target_url = `${BASE_HREF}/comic/${source_id}`
         let chapter_list = []
         let options = {
             'headers': {
@@ -53,7 +53,7 @@ export default class HaoManLiuService extends Base {
             },
             timeout: 3000,
         }
-        // 先拉头部
+
         await fetch(target_url, options)
             .then(res => res.text())
             .then(html => {
@@ -65,10 +65,12 @@ export default class HaoManLiuService extends Base {
                         let dom = li_dom_list.eq(i)
                         let link = dom.find("a").attr("href")
                         link = _this.getLink(link)
-                        let name = dom.find("p").text().trim()
+                        let name = dom.find("a").text().trim()
+                        let sequence = i + 1
                         let tmp_one = {
                             link,
                             name,
+                            sequence,
                         }
                         chapter_list.push(tmp_one)
                     }
@@ -102,10 +104,10 @@ export default class HaoManLiuService extends Base {
                 const $ = cheerio.load(html);
                 let image_object_list = $("img[data-echo]")
                 let image_length = image_object_list.length
-               for(let i=0;i< image_length; i++){
-                   let src = image_object_list.attr("data-echo")
-                   image_list.push(src)
-               }
+                for (let i = 0; i < image_length; i++) {
+                    let src = image_object_list.attr("data-echo")
+                    image_list.push(src)
+                }
                 // 破解结束
                 Log.ctxInfo(ctx, `拉取结束 target_url ${target_url} 总计图片数 ${image_list.length}`)
                 return image_list
