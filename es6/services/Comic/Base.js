7b98e13524404d59a8dce17fee7bdb396c4341c8
沐临风 <hlzblog@vip.qq.com>
2022-01-23 15:42:50 +0800

    update

--- es6/services/Comic/Base.js
+++ es6/services/Comic/Base.js
@@ -7,9 +7,9 @@ class Base extends BaseService {
     // http请求增加代理---前提配置了代理
     static getProxyOption(options) {
         if (undefined !== PROXY_DSN && PROXY_DSN.length > 0) {
-            options["agent"] = new getProxyHttpAgent({
+            options["agent"] = new getProxyHttpAgent({ // https://stackoverflow.com/questions/60061143/using-rejectunauthorized-with-node-fetch-in-node-js
                 proxy: PROXY_DSN,
-                rejectUnauthorized: false, // 以为
+                rejectUnauthorized: false, // 忽略https证书异常，对于爬虫来说无所谓，如果是敏感业务需要谨慎使用该选项
             });
             options["redirect"] = 'follow'
         }
