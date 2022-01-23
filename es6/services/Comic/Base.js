4ea990235ee27cc891b42c8a0a2d0f22f78b38bb
沐临风 <hlzblog@vip.qq.com>
2022-01-23 15:41:35 +0800

    Handle some of supplier https CA exception

--- es6/services/Comic/Base.js
+++ es6/services/Comic/Base.js
@@ -1,14 +1,19 @@
 import BaseService from '../../libs/Base/BaseService'
 import {PROXY_DSN} from "../../conf";
-const HttpProxyAgent = require('http-proxy-agent');
+// const HttpProxyAgent = require('http-proxy-agent');
+import { getProxyHttpAgent } from 'proxy-http-agent';
 
 class Base extends BaseService {
     // http请求增加代理---前提配置了代理
     static getProxyOption(options) {
         if (undefined !== PROXY_DSN && PROXY_DSN.length > 0) {
-            options["agent"] = new HttpProxyAgent(PROXY_DSN)
+            options["agent"] = new getProxyHttpAgent({
+                proxy: PROXY_DSN,
+                rejectUnauthorized: false, // 以为
+            });
             options["redirect"] = 'follow'
         }
+        console.log(options)
         return options
     }
 }
