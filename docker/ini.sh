8b739c065e9c0f41aed8098e988a04ede85261ae
沐临风 <hlzblog@vip.qq.com>
2022-06-18 23:47:38 +0800

    update

--- docker/ini.sh
+++ docker/ini.sh
@@ -10,12 +10,12 @@ cd /app \
 for((;;))
 do
     # 定时通知抓取
-    echo "推送中"
-    /usr/local/bin/node /app/dist/task comic notify_sub_all --conf="/app/app.yaml"
-    echo "推送成功"
     sleep_second=21600
     echo "正在挂起 ${sleep_second} 秒"
     sleep ${sleep_second}
+    echo "推送中"
+    /usr/local/bin/node /app/dist/task comic notify_sub_all --conf="/app/app.yaml"
+    echo "推送成功"
 done
 
 
