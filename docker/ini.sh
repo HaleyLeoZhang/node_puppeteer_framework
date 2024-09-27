080fe9024c68cb528a14e1447c6ba56e6a208d50
lex <hlzblog@vip.qq.com>
2024-09-28 00:31:54 +0800

    update

--- docker/ini.sh
+++ docker/ini.sh
@@ -15,15 +15,17 @@ cd /app \
 
 # 赋予权限，方便执行脚本
 chmod -R 755 /app/dist
-chmod -R 755 /app/docker/pm2_script
 # pm2 执行替代 supervisord
+chmod -R 755 /app/docker/pm2_script
+# 设置软链接
+ln -s  /app/node_modules/pm2/bin/pm2 /usr/local/bin/pm2
 # 爬虫
 /app/node_modules/pm2/bin/pm2 start -n base_consumer --restart-delay=3000 /app/docker/pm2_script/base_consumer.sh
-/app/node_modules/pm2/bin/pm2 start -n base_supplier_consumer --restart-delay=3000  //app/docker/pm2_script/base_supplier_consumer.sh
-/app/node_modules/pm2/bin/pm2 start -n supplier_chapter_consumer --restart-delay=3000  //app/docker/pm2_script/supplier_chapter_consumer.sh
-/app/node_modules/pm2/bin/pm2 start -n supplier_image_consumer --restart-delay=3000  //app/docker/pm2_script/supplier_image_consumer.sh
+/app/node_modules/pm2/bin/pm2 start -n base_supplier_consumer --restart-delay=3000  /app/docker/pm2_script/base_supplier_consumer.sh
+/app/node_modules/pm2/bin/pm2 start -n supplier_chapter_consumer --restart-delay=3000  /app/docker/pm2_script/supplier_chapter_consumer.sh
+/app/node_modules/pm2/bin/pm2 start -n supplier_image_consumer --restart-delay=3000  /app/docker/pm2_script/supplier_image_consumer.sh
 # www
-/app/node_modules/pm2/bin/pm2 start -n www --restart-delay=3000  //app/docker/pm2_script/www.sh
+/app/node_modules/pm2/bin/pm2 start -n www --restart-delay=3000  /app/docker/pm2_script/www.sh
 
 
 for((;;))
